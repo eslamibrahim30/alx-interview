d48d8900223781cb91568d0c28fe631195e0d245
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-03-08 04:27:24 +0200

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module is an implementation for
+Pascal's Triangle generator function"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [[1]]
+    else:
+        pascal_tri = list()
+        pascal_tri.append(list([1]))
+        for i in range(1, n):
+            current_row = list()
+            current_row.append(1)
+            for j in range(1, len(pascal_tri[-1])):
+                current_row.append(pascal_tri[-1][j] + pascal_tri[-1][j - 1])
+            current_row.append(1)
+            pascal_tri.append(current_row)
+        return pascal_tri
