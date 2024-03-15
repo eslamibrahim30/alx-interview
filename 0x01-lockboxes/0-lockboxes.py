da1f585dc259dc987fe8676dd0482f0ed8d33834
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-03-15 04:30:45 +0200

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,13 +12,17 @@ def canUnlockAll(boxes):
     unlocked.add(0)
     unchecked = set(range(len(boxes)))
     while len(unlocked) != len(boxes):
+        prev_unlocked = len(unlocked)
+        prev_unchecked = len(unchecked)
         checked = set()
         for i in unchecked:
             if i in unlocked:
                 checked.add(i)
                 for key in boxes[i]:
                     unlocked.add(key)
-        if not checked:
-            return False
         unchecked = unchecked.difference(checked)
+        cur_unlocked = len(unlocked)
+        cur_unchecked = len(unchecked)
+        if cur_unlocked == prev_unlocked and cur_unchecked == prev_unchecked:
+            return False
     return True
