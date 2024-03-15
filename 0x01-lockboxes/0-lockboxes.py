1ef3db3c62350bef6065a806389cb073e93fc759
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-03-15 04:00:41 +0200

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module is for task "Lockboxes"
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    This funciton determines if all the boxes can be unlocked
+    """
+    unlocked = set()
+    unlocked.add(0)
+    unchecked = set(range(len(boxes)))
+    while len(unlocked) != len(boxes):
+        checked = set()
+        previous_len = len(unlocked)
+        for i in unchecked:
+            if i in unlocked:
+                checked.add(i)
+                for key in boxes[i]:
+                    unlocked.add(key)
+        current_len = len(unlocked)
+        unchecked = unchecked.difference(checked)
+        if previous_len == current_len:
+            return False
+    return True
