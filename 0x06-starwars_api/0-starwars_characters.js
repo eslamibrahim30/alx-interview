0af6286b3ff3005a6e5b74177a6b018a02ee50c0
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-04-18 23:02:50 +0200

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const filmID = process.argv.slice(2)[0];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+request(url, function (error, response, body) {
+  if (error) {
+    throw error;
+  }
+  const chars = JSON.parse(body).results[parseInt(filmID) - 1].characters;
+  for (let i = 0; i < chars.length; i++) {
+    request(chars[i], function (error, response, body) {
+      if (error) {
+        throw error;
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
