edfccbe13b64a7773453b86d967f3791c1f95329
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-05-09 08:58:22 +0300

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module for task "Island Perimeter"
+"""
+from typing import List
+
+
+def island_perimeter(grid: List[List[int]]):
+    """
+    This function returns the perimeter of the island described in grid.
+    """
+    i_start = len(grid)
+    i_end = 0
+    j_start = len(grid)
+    j_end = 0
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 1:
+                count += 1
+                if i < i_start:
+                    i_start = i
+                if j < j_start:
+                    j_start = j
+                if i > i_end:
+                    i_end = i
+                if j > j_end:
+                    j_end = j
+    if count == 0:
+        return 0
+    if count == 1:
+        return 1
+    return ((j_end - j_start + 1) + (i_end - i_start + 1)) * 2
